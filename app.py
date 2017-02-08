918509f3380e527297d74e9641e05f1685bd23e8
Charles Simms Osborne <cosborn4@student03.cse.nd.edu>
2017-02-08 14:34:42 -0500

    app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/submitted', methods=['POST'])
+def submitted():
+	data = request.get_data().decode(encoding='UTF-8')
+	print data
+	return jsonify(data=data)
+
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
+
+
